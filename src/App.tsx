98c3940edaacdb825bab0634ca7a72ed4f697ad2
nick rodriguez <nick@catalytic.com>
2021-01-19 20:39:45 -0600

    feat(login-page): support login via api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -14,74 +14,77 @@ import getServerURL from './serverOverride';
 import LoginPage from './modules/LoginPage';
 import LandingPage from './modules/LandingPages/LandingPage';
 
-interface State {
-  isLoggedIn: boolean;
-}
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<{ firstName?: string; lastName?: string }>({});
 
-class App extends React.Component<{}, State, {}> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      isLoggedIn: true,
-    };
-  }
-
-  setLoginState = (isLoggedIn: boolean) => {
-    this.setState({ isLoggedIn });
-    if (!isLoggedIn) {
+  const handleLoginStateChange = (state: boolean) => {
+    setIsLoggedIn(state);
+    if (!state) {
       fetch(`${getServerURL()}/logout`, {
         method: 'GET',
         credentials: 'include',
       });
+    } else {
+      fetch(`${getServerURL()}/get-user-info`, {
+        method: 'POST',
+        credentials: 'include',
+      })
+        .then((r) => r.json())
+        .then((userInfoResponseBody) => {
+          if (userInfoResponseBody.status === 'SUCCESS') {
+            setUser(userInfoResponseBody);
+          }
+        });
     }
   };
 
-  render() {
-    const { isLoggedIn } = this.state;
-    return (
-      <Router>
-        <div className="App">
-          <div className="app">
-            <Helmet>
-              <title>Keep.id</title>
-              <meta
-                name="description"
-                content="Securely Combating Homelessness"
-              />
-            </Helmet>
-            <Header
-              isLoggedIn={isLoggedIn}
-              setLoginState={this.setLoginState}
+  return (
+    <Router>
+      <div className="App">
+        <div className="app">
+          <Helmet>
+            <title>Keep.id</title>
+            <meta
+              name="description"
+              content="Securely Combating Homelessness"
             />
-            <Switch>
-              <Route exact path="/" render={() => <Redirect to="/login" />} />
-              <Route
-                path="/home"
-                render={() => (isLoggedIn ? <LandingPage firstName="Nick" lastName="Rodriguez" /> : <Redirect to="/login" />)}
-              />
-              <Route
-                path="/login"
-                render={() => (isLoggedIn ? (
-                  <Redirect to="/home" />
-                ) : (
-                  <LoginPage
-                    isLoggedIn={isLoggedIn}
-                    setLoginState={this.setLoginState}
-                  />
-                ))}
-              />
-              <Route path="/error">
-                <Error />
-              </Route>
-              <Route>
-                <Redirect to="/error" />
-              </Route>
-            </Switch>
-          </div>
-          <Footer />
+          </Helmet>
+          <Header
+            isLoggedIn={isLoggedIn}
+            setLoginState={handleLoginStateChange}
+          />
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route
+              path="/home"
+              render={() => (isLoggedIn
+                ? <LandingPage firstName={user.firstName || ''} lastName={user.lastName || ''} />
+                : <Redirect to="/login" />)}
+            />
+            <Route
+              path="/login"
+              render={() => (isLoggedIn ? (
+                <Redirect to="/home" />
+              ) : (
+                <LoginPage
+                  isLoggedIn={isLoggedIn}
+                  setLogInState={handleLoginStateChange}
+                />
+              ))}
+            />
+            <Route path="/error">
+              <Error />
+            </Route>
+            <Route>
+              <Redirect to="/error" />
+            </Route>
+          </Switch>
         </div>
-      </Router>
-    );
-  }
-}
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
 export default App;
--- src/modules/LoginPage.tsx
+++ src/modules/LoginPage.tsx
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { withAlert } from 'react-alert';
 import ReCAPTCHA from 'react-google-recaptcha';
 import getServerURL from '../serverOverride';
 import LoginSVG from '../static/images/login-svg.svg';
 import { reCaptchaKey } from '../configVars';
-
-interface State {
-  username: string,
-  password: string,
-  buttonState: string,
-  recaptchaPayload: string
-}
 
 const recaptchaRef: React.RefObject<ReCAPTCHA> = React.createRef();
 
@@ -21,136 +14,131 @@ interface Props {
   alert: any
 }
 
-class LoginPage extends Component<Props, State> {
-  static enterKeyPressed(event, funct) {
-    if (event.key === 'Enter') {
-      funct();
+const LoginPage = ({ alert, isLoggedIn, setLogInState }: Props) => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [buttonState, setButtonState] = useState<string>();
+  const [recaptchaPayload, setRecaptchaPayload] = useState<string>();
+
+  const handleLogin = async () => {
+    const loginResponse = await fetch(`${getServerURL()}/login`, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    const loginResponseBody = await loginResponse.json();
+    if (loginResponse.ok && loginResponseBody.status === 'AUTH_SUCCESS') {
+      alert.show('Login successful!', { type: 'success' });
+      setLogInState(true);
+      return;
     }
-  }
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      buttonState: '',
-      recaptchaPayload: '',
-    };
-  }
+    let errorMessage = 'Login failed!';
+    if (loginResponseBody.status === 'USER_NOT_FOUND') {
+      errorMessage = 'Incorrect Username!';
+    } else if (loginResponseBody.status === 'AUTH_FAILURE') {
+      errorMessage = 'Incorrect Password!';
+    }
 
-  // event handlers- whenever an action happens, update the state
-  handleChangePassword = (event: any) => {
-    this.setState({ password: event.target.value });
-  }
+    alert.show(errorMessage, { type: 'error' });
+    setLogInState(false);
+  };
 
-  handleChangeUsername = (event: any) => {
-    this.setState({ username: event.target.value });
-  }
-
-  // recaptcha specific code: see https://github.com/dozoisch/react-google-recaptcha
-  onSubmitWithReCAPTCHA = async (e) => {
+  const onSubmitWithReCAPTCHA = async (e) => {
     e.preventDefault();
     if (recaptchaRef !== null && recaptchaRef.current !== null) {
-      const recaptchaPayload = await recaptchaRef.current.executeAsync();
-      this.setState({ recaptchaPayload }, this.handleLogin);
+      const payload = await recaptchaRef.current.executeAsync();
+      setRecaptchaPayload(payload);
     }
-  }
+    return handleLogin();
+  };
 
-  resetRecaptcha = (): void => {
-    if (recaptchaRef !== null && recaptchaRef.current !== null) {
-      recaptchaRef.current.reset();
-    }
-    this.setState({ recaptchaPayload: '' });
-  }
-
-  handleLogin = (): void => {
-    // TODO in front end training module
-  }
-
-  render() {
-    const {
-      username,
-      password,
-    } = this.state;
-    return (
-      <div>
-        <Helmet>
-          <title>Login</title>
-          <meta name="description" content="Keep.id" />
-        </Helmet>
-        <div className="container">
-          <div className="row mt-4">
-            <div className="col mobile-hide">
-              <div className="float-right w-100">
-                <img alt="Login graphic" className="w-75 pt-5 mt-5 mr-3 float-right " src={LoginSVG} />
-              </div>
-              <div className="row pl-3 pb-3">
-                <span className="text-muted recaptcha-login-text pt-4 mt-4 pl-5 ml-5 w-75">
-                  This page is protected by reCAPTCHA, and subject to the Google
-                  {' '}
-                  <a href="https://www.google.com/policies/privacy/">Privacy Policy </a>
-                  and
-                  {' '}
-                  <a href="https://www.google.com/policies/terms/">Terms of service</a>
-                  .
-                </span>
-              </div>
+  return (
+    <div>
+      <Helmet>
+        <title>Login</title>
+        <meta name="description" content="Keep.id" />
+      </Helmet>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col mobile-hide">
+            <div className="float-right w-100">
+              <img alt="Login graphic" className="w-75 pt-5 mt-5 mr-3 float-right " src={LoginSVG} />
             </div>
-
-            <div className="col">
-              <form className="form-signin pt-5">
-                <h1 className="h3 mb-3 font-weight-normal">Sign in</h1>
-                <label htmlFor="username" className="w-100 font-weight-bold">
-                  Username
-                  <input
-                    type="text"
-                    className="form-control form-purple mt-1"
-                    id="username"
-                    placeholder="username"
-                    value={username}
-                    onChange={this.handleChangeUsername}
-                    required
-                  />
-                </label>
-                <label htmlFor="password" className="w-100 pt-2 font-weight-bold">
-                  Password
-                  <input
-                    type="password"
-                    className="form-control form-purple mt-1"
-                    id="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={this.handleChangePassword}
-                    required
-                  />
-                </label>
-              </form>
-              <div className="row pl-3 pt-3">
-                <div className="pb-2">
-                  <button
-                    type="submit"
-                    onKeyDown={(e) => LoginPage.enterKeyPressed(e, this.onSubmitWithReCAPTCHA)}
-                    onClick={this.onSubmitWithReCAPTCHA}
-                    className={`btn btn-success px-5 loginButtonBackground w-100 ld-ext-right ${this.state.buttonState}`}
-                  >
-                    Sign In
-                    <div className="ld ld-ring ld-spin" />
-                  </button>
-                </div>
-              </div>
+            <div className="row pl-3 pb-3">
+              <span className="text-muted recaptcha-login-text pt-4 mt-4 pl-5 ml-5 w-75">
+                This page is protected by reCAPTCHA, and subject to the Google
+                {' '}
+                <a href="https://www.google.com/policies/privacy/">Privacy Policy </a>
+                and
+                {' '}
+                <a href="https://www.google.com/policies/terms/">Terms of service</a>
+                .
+              </span>
             </div>
-
           </div>
-          <ReCAPTCHA
-            theme="dark"
-            size="invisible"
-            ref={recaptchaRef}
-            sitekey={reCaptchaKey}
-          />
+          <div className="col">
+            <form className="form-signin pt-5">
+              <h1 className="h3 mb-3 font-weight-normal">Sign in</h1>
+              <label htmlFor="username" className="w-100 font-weight-bold">
+                Username
+                <input
+                  type="text"
+                  className="form-control form-purple mt-1"
+                  id="username"
+                  placeholder="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </label>
+              <label htmlFor="password" className="w-100 pt-2 font-weight-bold">
+                Password
+                <input
+                  type="password"
+                  className="form-control form-purple mt-1"
+                  id="password"
+                  placeholder="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </label>
+            </form>
+            <div className="row pl-3 pt-3">
+              <div className="pb-2">
+                <button
+                  type="submit"
+                  onKeyDown={LoginPage.onEnterKeyPressed(onSubmitWithReCAPTCHA)}
+                  onClick={onSubmitWithReCAPTCHA}
+                  className={`btn btn-success px-5 loginButtonBackground w-100 ld-ext-right ${buttonState}`}
+                >
+                  Sign In
+                  <div className="ld ld-ring ld-spin" />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+        <ReCAPTCHA
+          theme="dark"
+          size="invisible"
+          ref={recaptchaRef}
+          sitekey={reCaptchaKey}
+        />
       </div>
-    );
+    </div>
+  );
+};
+
+LoginPage.onEnterKeyPressed = (fn) => (e) => {
+  if (e.key === 'Enter') {
+    fn(e);
   }
-}
+};
 
 export default withAlert()(LoginPage);
