1179e98cc29445574033f793951e443c643311ec
jzhang0107 <zhang98@bu.edu>
2021-04-13 19:07:49 -0400

    progress bar and buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
+  BrowserRouter as Router, Redirect, Route, Switch,
 } from 'react-router-dom';
 import './static/styles/App.scss';
 import { Helmet } from 'react-helmet';
@@ -15,73 +12,74 @@ import LoginPage from './components/LoginPage';
 import LandingPage from './components/LandingPages/LandingPage';
 
 interface State {
-  isLoggedIn: boolean;
+    isLoggedIn: boolean;
 }
 
 class App extends React.Component<{}, State, {}> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      isLoggedIn: false,
+      isLoggedIn: true,
     };
   }
 
-  setLoginState = (isLoggedIn: boolean) => {
-    this.setState({ isLoggedIn });
-    if (!isLoggedIn) {
-      fetch(`${getServerURL()}/logout`, {
-        method: 'GET',
-        credentials: 'include',
-      });
-    }
-  };
+    setLoginState = (isLoggedIn: boolean) => {
+      this.setState({ isLoggedIn });
+      if (!isLoggedIn) {
+        fetch(`${getServerURL()}/logout`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+      }
+    };
 
-  render() {
-    const { isLoggedIn } = this.state;
-    return (
-      <Router>
-        <div className="App">
-          <div className="app">
-            <Helmet>
-              <title>Keep.id</title>
-              <meta
-                name="description"
-                content="Securely Combating Homelessness"
+    render() {
+      const { isLoggedIn } = this.state;
+      return (
+        <Router>
+          <div className="App">
+            <div className="app">
+              <Helmet>
+                <title>Keep.id</title>
+                <meta
+                  name="description"
+                  content="Securely Combating Homelessness"
+                />
+              </Helmet>
+              <Header
+                isLoggedIn={isLoggedIn}
+                setLoginState={this.setLoginState}
               />
-            </Helmet>
-            <Header
-              isLoggedIn={isLoggedIn}
-              setLoginState={this.setLoginState}
-            />
-            <Switch>
-              <Route exact path="/" render={() => <Redirect to="/login" />} />
-              <Route
-                path="/home"
-                render={() => (isLoggedIn ? <LandingPage /> : <Redirect to="/login" />)}
-              />
-              <Route
-                path="/login"
-                render={() => (isLoggedIn ? (
-                  <Redirect to="/home" />
-                ) : (
-                  <LoginPage
-                    isLoggedIn={isLoggedIn}
-                    setLoginState={this.setLoginState}
-                  />
-                ))}
-              />
-              <Route path="/error">
-                <Error />
-              </Route>
-              <Route>
-                <Redirect to="/error" />
-              </Route>
-            </Switch>
+              <Switch>
+                <Route exact path="/" render={() => <Redirect to="/login" />} />
+                <Route
+                  path="/home"
+                  render={() => (isLoggedIn ? <LandingPage /> : <Redirect to="/login" />)}
+                />
+                <Route
+                  path="/login"
+                  render={() => (isLoggedIn ? (
+                    <Redirect to="/home" />
+                  ) : (
+                    <LoginPage
+                      isLoggedIn={isLoggedIn}
+                      setLoginState={this.setLoginState}
+                    />
+                  ))}
+                />
+                <Route path="/error">
+                  <Error />
+                </Route>
+                <Route>
+                  <Redirect to="/error" />
+                </Route>
+              </Switch>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+        </Router>
+      );
+    }
 }
+
 export default App;
--- src/components/LandingPages/LandingPage.tsx
+++ src/components/LandingPages/LandingPage.tsx
@@ -9,7 +9,27 @@ class ClientLanding extends Component<{}, {}, {}> {
           <title>Home</title>
           <meta name="description" content="Keep.id" />
         </Helmet>
-        <h1>Welcome to the Landing Page</h1>
+
+        {/* Header */}
+        <h1>Welcome Jason</h1>
+
+        {/* Progress bar */}
+        <div className="progress">
+          <div className="progress-bar w-50" role="progressbar" />
+        </div>
+
+        {/* Prev/Next step buttons */}
+        <div className="progress-bar-btn-container">
+          <button type="submit" className="btn btn-outline-primary">Previous Step</button>
+          <button type="submit" className="btn btn-outline-primary">Next Step</button>
+        </div>
+
+        {/* Error/Success/Add Card buttons */}
+        <div className="button-container">
+          <button type="submit" className="btn btn-primary">Error Message</button>
+          <button type="submit" className="btn btn-primary">Success Toast</button>
+          <button type="submit" className="btn btn-primary">Add Card</button>
+        </div>
       </div>
     );
   }
